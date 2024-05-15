6f28d1a7dcad3402e2d2598f5cea84e5a67a420f
Meet Patel <meetpatel01997@gmail.com>
2024-05-15 17:06:40 +0530

    [MP - timmer game full project]

---
+++ src/components/Player.jsx
@@ -0,0 +1,18 @@
+import { useState, useRef } from "react";
+
+export default function Player() {
+  const enteredPlayerName = useRef();
+  const [playerName, setPlayerName] = useState() 
+  const handleClick = () => {
+    setPlayerName(enteredPlayerName.current.value)
+  }
+  return (
+    <section id="player">
+      <h2>Welcome {playerName ?? 'unknown entity'}</h2>
+      <p>
+        <input ref={enteredPlayerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
